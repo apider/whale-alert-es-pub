29e50abf0944a1534a2296328f3f590c73d2ba35
Fille <filipk73@gmail.com>
2022-10-08 00:49:20 +0200

    .

--- app.py
+++ app.py
@@ -51,6 +51,12 @@ def getWhaleData(cursor):
         with httpx.Client() as sync_client:
             r = sync_client.get(url=URL + TRANSACTIONURI, params=params)
             return r.json()
+    except httpx.TimeoutException as e:
+        logging.error(e)
+        return False
+    except httpx.ConnectTimeout as e:
+        logging.error(e)
+        return False
     except httpx.ReadTimeout as e:
         logging.error(e)
         return False
